f41bc50694b780d4bd31c171e03a4a4a7d495144
Sean-HL-Lin <hongshenglin92@gmail.com>
2019-10-30 19:26:58 +0000

    add client side funtion ffor deleting cities

--- final-client/src/components/Home/CityCard.js
+++ final-client/src/components/Home/CityCard.js
@@ -2,6 +2,7 @@ import React from 'react';
 // eslint-disable-next-line
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "../../styles/Home.css";
+import DeleteCity from '../../helpers/DeleteCity'
 
 export default function CityCard(props) {
 
@@ -19,7 +20,10 @@ export default function CityCard(props) {
             className="btn btn-light"
             style={{ position: 'absolute', zIndex: '1', top:'0px', right: '0px',borderRadius:'15px', backgroundColor:"rgba(0,0,0,0)", color:"white", fontWeight: 'bold', border: '0px'}}
             onClick={() => {
-              console.log('to be deleted')
+              const id = props.city.id
+              DeleteCity(id).then(() => {
+                console.log('city deleted?')
+              })
             }}
           >
             X
